67051c17c5fa5774c55c9472aab3cc07db66548a
Cami <jadeshades12@gmail.com>
2026-01-18 11:56:43 +0800

    added an env file for the firebase

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA5nwiDPQEAOdRhMaPk1pVTbPZfwaT4G7M",
-  authDomain: "eco-racer-326a9.firebaseapp.com",
-  projectId: "eco-racer-326a9",
-  storageBucket: "eco-racer-326a9.firebasestorage.app",
-  messagingSenderId: "159065584702",
-  appId: "1:159065584702:web:cc86ca6047f76bc9494d6f",
-  measurementId: "G-DF0CFWTQX3"
+  apiKey: "REACT_APP_FIREBASE_API_KEY",
+  authDomain: "REACT_APP_FIREBASE_AUTH_DOMAIN",
+  projectId: "REACT_APP_FIREBASE_PROJECT_ID",
+  storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "REACT_APP_FIREBASE_APP_ID",
+  measurementId: "REACT_APP_FIREBASE_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
